d4aeba7519fb4d86967d2da1799d6c06c3af9d9f
Gabriel-Aguiar-dev <gabriel.aghot22@gmail.com>
2022-12-18 14:41:03 -0300

    Atualizacao feita

--- src/cursojava/Aula08ArraysMultidimensionais01.java
+++ src/cursojava/Aula08ArraysMultidimensionais01.java
@@ -18,6 +18,14 @@ public class Aula08ArraysMultidimensionais01 {
 			for (int j = 0; j < dias[0].length; j++) {
 				System.out.println(dias[i][j]);
 			}
+
+			System.out.println("----------A MESMA COISA-----------");
+
+			for(int[]arrBase:dias){
+				for(int num : arrBase){
+					System.out.println(num);
+				}
+			}
 		}
 	}
 }
